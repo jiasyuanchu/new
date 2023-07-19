3e34eba7852f45cb469d32438b63cd71bc142f18
CH Chu <120808457+jiasyuanchu@users.noreply.github.com>
2023-07-19 16:39:26 +0800

    feat: install fast-xml-parser & parse gpx data

--- app.js
+++ app.js
@@ -7,37 +7,53 @@ const gpxParse = require("gpx-parse")
 const app = express()
 const PORT = 3000
 
-app.engine('hbs', exphbs({ defaultLayout: 'main', extname: '.hbs' }))
-app.set('view engine', 'hbs')
-app.use(express.urlencoded({ extended: true }))
-app.use(methodOverride('_method'))
-app.get('/', (req, res) => {
-  res.send('hello world')
-})
+// app.engine('hbs', exphbs({ defaultLayout: 'main', extname: '.hbs' }))
+// app.set('view engine', 'hbs')
+// app.use(express.urlencoded({ extended: true }))
+// app.use(methodOverride('_method'))
+// app.get('/', (req, res) => {
+//   res.send('hello world')
+// })
 
-//操作gpx-parse
-let parsedData; // 儲存解析後的資料
+//操作fast-xml-parser
+const fs = require('fs');
+const { XMLParser } = require("fast-xml-parser");
 
-gpxParse.parseGpxFromFile('./path/2023_07_15.gpx', function (error, data) {
-  if (error) {
-    console.error(error);
-    return;
+const gpxData = fs.readFileSync('./path/2023_07_15.gpx', 'utf8');
+const options = {
+  attributeNamePrefix: '',
+  ignoreAttributes: false,
+  parseAttributeValue: true
+};
+
+const parser = new XMLParser(options);
+const jsonObj = parser.parse(gpxData);
+const waypoints = jsonObj.gpx.trk.trkseg.trkpt;
+
+const parsedData = waypoints.map(waypoint => {
+  return {
+    latitude: waypoint.lat,
+    longitude: waypoint.lon,
+    elevation: waypoint.ele,
+    time: waypoint.time
   }
-  const waypoints = data.tracks[0].segments[0];
-  parsedData = waypoints.map(waypoint => {
-    return {
-      latitude: waypoint.lat,
-      longitude: waypoint.lon,
-      elevation: waypoint.ele,
-      time: waypoint.time
-    };
-  });
-  console.log(parsedData); // 確認資料正確解析
-});
+})
+console.log(parsedData) //印出json格式的回傳資料
 
-app.get('/coordinates', (req, res) => {
-  res.json(parsedData); // 回傳解析後的資料給前端
-});
+//將回傳資料轉為前端指定的array格式
+const waypointsArray = []
+
+for (const waypoint of parsedData) {
+  const waypointArray = [
+    waypoint.latitude,
+    waypoint.longitude,
+    waypoint.elevation,
+    waypoint.time
+  ];
+
+  waypointsArray.push(waypointArray)
+}
+console.log(waypointsArray)//此為前端希望的回傳格式
 
 
 app.listen(PORT, () => {
