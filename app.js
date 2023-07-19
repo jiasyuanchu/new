723c63f2b73484ed7d8703e180e98544af1f2f33
CH Chu <120808457+jiasyuanchu@users.noreply.github.com>
2023-07-19 15:39:40 +0800

    feat: try gpx-parse but fail on elevation data

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ const express = require('express')
 const exphbs = require('express-handlebars')
 const methodOverride = require('method-override')
 const bcrypt = require('bcryptjs')
+const gpxParse = require("gpx-parse")
 
 const app = express()
 const PORT = 3000
@@ -13,6 +14,31 @@ app.use(methodOverride('_method'))
 app.get('/', (req, res) => {
   res.send('hello world')
 })
+
+//操作gpx-parse
+let parsedData; // 儲存解析後的資料
+
+gpxParse.parseGpxFromFile('./path/2023_07_15.gpx', function (error, data) {
+  if (error) {
+    console.error(error);
+    return;
+  }
+  const waypoints = data.tracks[0].segments[0];
+  parsedData = waypoints.map(waypoint => {
+    return {
+      latitude: waypoint.lat,
+      longitude: waypoint.lon,
+      elevation: waypoint.ele,
+      time: waypoint.time
+    };
+  });
+  console.log(parsedData); // 確認資料正確解析
+});
+
+app.get('/coordinates', (req, res) => {
+  res.json(parsedData); // 回傳解析後的資料給前端
+});
+
 
 app.listen(PORT, () => {
   console.log(`App is running on http://localhost:${PORT}`)
